d41d92590d82de61cd8be1bf131bf5e91ee02fe3
Tyler Knapp <tylerknapp25@gmail.com>
2022-08-30 19:37:32 -0400

    working on FileReaders

--- 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
+++ 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
@@ -2,11 +2,15 @@ package com.bobocode.se;
 
 import com.bobocode.util.ExerciseNotCompletedException;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * {@link FileReaders} provides an API that allow to read whole file into a {@link String} by file name.
  */
 public class FileReaders {
-
+    
     /**
      * Returns a {@link String} that contains whole text from the file specified by name.
      *
@@ -14,6 +18,17 @@ public class FileReaders {
      * @return string that holds whole file content
      */
     public static String readWholeFile(String fileName) {
-        throw new ExerciseNotCompletedException(); //todo
+        try {
+            File file = new File(fileName);
+            Scanner in = new Scanner(file);
+            while (in.hasNextLine()) {
+                return in.nextLine();
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Something went wrong");
+            e.printStackTrace();
+        }
+        return "";
     }
 }
